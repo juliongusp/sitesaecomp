6ebc272f116caeeaaa1e40cbd831b7c1ce959e99
juliongusp <juliong97@gmail.com>
2017-11-11 17:52:36 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE HTML>
+<!--
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>SAEComp</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper" class="wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+							<div id="logo">
+								<h1><a href="index.html">SAEComp</a></h1>
+								<p>Secretaria Acadêmica da Engenharia de Computação</p>
+								<br>
+								<br>
+								<img src="images/Simbolo.png" width='700' height='700'>
+							</div>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									
+									<li><a href="#myAnchor2" rel="" id="anchor2" class="anchorLink">Sobre Nós</a></li>
+									<li><a href="#myAnchor3" rel="" id="anchor3" class="anchorLink">Equipe</a></li>
+									<li><a href="#myAnchor4" rel="" id="anchor4" class="anchorLink">Relacionados</a></li>
+									<li><a href="#myAnchor5" rel="" id="anchor5" class="anchorLink">Se interessou?</a></li>
+									<li><a href="left-sidebar.html">Painel do Bixo</a></li>
+								</ul>
+							</nav>
+
+					</div>
+				</div>
+
+			<!-- Intro -->
+				<div id="intro-wrapper" class="wrapper style1">
+					<div class="title"><a name="myAnchor2" id="myAnchor2">Sobre nós</a></div>
+					<section id="intro" class="container">
+						<p class="style1">No caso de estiver se perguntando sobre o que é tudo isso...</p>
+						<p class="style2">
+							SAEComp é a Secretaria Acadêmica da Engenharia de Computação!<br class="mobile-hide" />
+						</p>
+						<p class="style3">Somos aqueles que se dedicam por <strong>representar </strong>os alunos da<strong> Engenharia de Computação</strong> da <strong>Universidade de São Paulo</strong> - campus São Carlos -
+						a partir de <strong>projetos</strong>, <strong>eventos</strong>, <strong>produtos</strong> e <strong>juridicamente</strong> perante a Universidade e ao curso
+						<br><br>
+						<center><img src="images/Simbolo.png" width='100' height='100'></center>
+					</section>
+				</div>
+
+			<!-- Main -->
+				<div class="wrapper style1" style="background-color:#34495E">
+					<div class="title" style="background-color:#34495E"><a name="myAnchor3" id="myAnchor3">A Equipe</a></div>
+					<div id="main" class="container">
+
+						<h2><center><font size="30" color="#FFFFFF">"Coisas incríveis no mundo nunca são feitas por uma única
+							pessoa, e sim, por uma equipe"</font> <br><br> <font size="6">-Steve Jobs</font>
+						</center></h2>
+						<br>
+						</section>
+
+						<!-- Image -->
+							<a href="#" class="image featured">
+								<img src="images/timesaecomp.jpg" />
+							</a>
+
+						<!-- Features -->
+							<section id="features">
+								<header class="style1">
+									<h2>Diretoria</h2>
+									<p>Gestão Sucessão</p>
+								</header>
+								<div class="feature-list">
+									<div class="row">
+										<div class="6u 12u(mobile)">
+											<section>
+												<center>
+												<h3>Presidente</h3><br>
+												<h3><img style="border-radius:50%" src="images/presidente.jpg" width="25%" height="25%"></h3>
+												Borges
+												</center>
+											</section>
+										</div>
+										<div class="6u 12u(mobile)">
+											<section>
+												<center>
+												<h3>Vice Presidente</h3><br>
+												<h3><img style="border-radius:50%" src="images/vicepresidente.jpg" width="25%" height="25%"></h3>
+												Turbo
+												</center>
+											</section>
+										</div>
+									</div>
+									<div class="row">
+										<div class="6u 12u(mobile)">
+											<section>
+												<center>
+												<h3>Diretor Financeiro</h3><br>
+												<h3><img style="border-radius:50%" src="images/financeiro.jpg" width="25%" height="25%"></h3>
+												Fusão
+												</center>
+											</section>
+										</div>
+										<div class="6u 12u(mobile)">
+											<section>
+												<center>
+												<h3>Diretor de Marketing</h3><br>
+												<h3><img style="border-radius:50%" src="images/marketing.jpg" width="25%" height="25%"></h3>
+												Desmond
+												</center>
+											</section>
+										</div>
+									</div>
+									<div class="row">
+											<div class="6u 12u(mobile)">
+													<section>
+														<center>
+														<h3>Diretor de Projetos</h3><br>
+														<h3><img style="border-radius:50%" src="images/projetos.jpg" width="25%" height="25%"></h3>
+														Beto
+														</center>
+													</section>
+										</div>
+										<div class="6u 12u(mobile)">
+												<section>
+													<center>
+													<h3>Diretor de GP</h3><br>
+													<h3><img style="border-radius:50%" src="images/gp.jpg" width="25%" height="25%"></h3>
+													Máscara
+													</center>
+												</section>
+										</div>
+									</div>
+								</div>
+							</section>
+
+					</div>
+				</div>
+
+			<!-- Highlights -->
+				<div class="wrapper style3">
+					<div class="title"><a name="myAnchor4" id="myAnchor4">Relacionados</a></div>
+					<div id="highlights" class="container">
+						<div class="row 150%">
+							<div class="4u 12u(mobile)">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/senc.png" alt="" /></a>
+									<h3><a href="#">SEnC</a></h3>
+									<p>A SEnC - Semana da Engenharia de Computação - é realizada todo ano com o apoio de gestão dos membros da SAEComp, o evento busca introduzir palestras e cursos que envolvem a Engenharia de Computação.</p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Saiba Mais</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/SimboloInverse.png" alt="" /></a>
+									<h3><a href="#">Facebook</a></h3>
+									<p>A página da SAEComp no facebook disponibiliza notícias sobre o curso; além de eventos e projetos realizados pela Secretaria acadêmica e seus membros. Fique ligado sobre nossas atividades!</p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Saiba Mais</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/caaso.png" alt="" /></a>
+									<h3><a href="#">CAASO</a></h3>
+									<p>O Centro acadêmico Armando Salles de Oliveira representa todos os alunos do Campus da USP São Carlos e dá o apoio necessário para todas as Secretarias Acadêmicas.</p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Saiba Mais</a></li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer-wrapper" class="wrapper">
+					<div class="title"><a name="myAnchor5" id="myAnchor5">Se interessou?</a></div>
+					<div id="footer" class="container">
+						<header class="style1">
+							<h2>Curtiu a Engenharia de Computação? A Secretaria Acadêmica? Os Projetos e Eventos?</h2>
+							<p>
+								Mande uma mensagem para a SAEComp nas redes sociais e sinta-se a vontade para contatar qualquer um de nossos membros!
+							</p>
+						</header>
+						<hr />
+						<div class="row 150%">
+							<center>
+							<div class="6u 12u(mobile)">
+
+					<img src="images/saecomp1.png" width="70%" height="70%">
+					<div>
+					<div id="copyright">
+						<ul>
+							<li>&copy; Julio Ng</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</div>
+					</div>
+				</div>
+				</center>
+		</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ left-sidebar.php
@@ -0,0 +1,298 @@
+<!DOCTYPE HTML>
+<!--
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>SAEComp</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="left-sidebar">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper" class="wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+						<div id="logo">
+							<h1><a href="index.html">SAEComp</a></h1>
+							<p>Secretaria Acadêmica da Engenharia de Computação</p>
+							<br>
+						</div>
+
+						<!-- Nav -->
+						<nav id="nav">
+								<ul>
+									<li><a href="index.html">Página Principal</a></li>
+								</ul>
+							</nav>
+
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div class="wrapper style1">
+					<div class="title">Painel do Bixo</div>
+					<div id="main" class="container">
+						<div class="row 150%">
+							<div class="4u 12u(mobile)">
+
+								<!-- Sidebar -->
+									<div id="sidebar">
+										<section class="box">
+											<header>
+												<h2>Perdido?</h2>
+											</header>
+											<p>Fique atento as nossas redes sociais e procure um veterano! Estaremos sempre dispostos
+												a ajudar no que for preciso!
+											</p>
+										</section>
+										<section class="box">
+											<header>
+												<h2>Os Institutos:</h2>
+											</header>
+											<ul class="style2">
+												<li>
+													<article class="box post-excerpt">
+														<a href="#" class="image left"><img src="images/pic08.jpg" alt="" /></a>
+														<h3><a href="#">EESC</a></h3>
+														<p>Escola de Engenharia de São Carlos</p>
+													</article>
+												</li>
+												<li>
+													<article class="box post-excerpt">
+														<a href="#" class="image left"><img src="images/pic09.jpg" alt="" /></a>
+														<h3><a href="#">ICMC</a></h3>
+														<p>Instituto de Ciências Matemáticas e da Computação</p>
+													</article>
+												</li>
+												<li>
+													<article class="box post-excerpt">
+														<a href="#" class="image left"><img src="images/pic10.jpg" alt="" /></a>
+														<h3><a href="#">IFSC</a></h3>
+														<p>Instituto de Física de São Carlos</p>
+													</article>
+												</li>
+												<li>
+														<article class="box post-excerpt">
+															<a href="#" class="image left"><img src="images/pic10.jpg" alt="" /></a>
+															<h3><a href="#">IQSC</a></h3>
+															<p>Instituto de Química de São Carlos</p>
+														</article>
+													</li>
+													<li>
+															<article class="box post-excerpt">
+																<a href="#" class="image left"><img src="images/pic10.jpg" alt="" /></a>
+																<h3><a href="#">IAU</a></h3>
+																<p>Instituo de Arquitetura e Urbanismo</p>
+															</article>
+														</li>
+											</ul>
+										</section>
+										<section class="box">
+											<header>
+												<h2>Os Extracurriculares</h2>
+											</header>
+											<ul class="style3"><strong>
+												<li><a href="#">SAEComp.</a></li>
+												<li><a href="#">SEnC</a></li>
+												<li><a href="#">EESC Jr.</a></li>
+												<li><a href="#">ICMC Jr.</a></li>
+												<li><a href="#">ADA</a></li>
+												<li><a href="#">Warthog</a></li>
+												<li><a href="#">FOG</a></li>
+												<li><a href="#">Fórmula SAE</a></li>
+												<li><a href="#">BAJA</a></li>
+												<li><a href="#">TUPÃ</a></li>
+												<li><a href="#">ENACTUS</a></li>
+												</strong>
+											</ul>
+										</section>
+									</div>
+
+							</div>
+							<div class="8u 12u(mobile) important(mobile)">
+
+								<!-- Content -->
+									<div id="content">
+										<article class="box post">
+											<header class="style1">
+												<h2>Bem vindo a Universidade!</h2>
+												<p>Preparamos um guia rápido para você!</p>
+											</header>
+											<a href="#" class="image featured">
+												<img src="images/pic01.jpg" alt="" />
+											</a>
+											<p>Seu sorriso é tão resplandecente
+													Que deixou meu coração alegre
+													Me dê a mão
+													Para fugir dessa terrível escuridão
+													
+													Desde o dia em que te reencontrei
+													Me lembrei daquele lindo lugar
+													Que na minha infância era
+													Especial para mim
+													
+													Quero saber
+													Se comigo você quer vir dançar
+													Se me der a mão eu te levarei
+													Por um caminho cheio de sombras e de luz
+													
+													</p>
+											<p>Você pode até não perceber
+													Mas o meu coração se amarrou em você
+													Que precisa de alguém
+													Pra te mostrar o amor que o mundo te da
+													
+													Meu alegre coração palpita
+													Por um universo de esperança
+													Me dê a mão
+													A magia nos espera
+													
+													Vou te amar por toda a minha vida
+													Vem comigo por este caminho
+													Me dê a mão
+													Pra fugir desta terrível escuridão</p>
+											<p>Seu sorriso é tão resplandecente
+													Que deixou meu coração alegre
+													Me dê a mão
+													Para fugir dessa terrível escuridão
+													
+													Desde o dia em que te reencontrei
+													Me lembrei daquele lindo lugar
+													Que na minha infância era
+													Especial para mim
+													
+													Quero saber
+													Se comigo você quer vir dançar
+													Se me der a mão eu te levarei
+													Por um caminho cheio de sombras e de luz</p>
+											<p>Você pode até não perceber
+													Mas o meu coração se amarrou em você
+													Que precisa de alguém
+													Pra te mostrar o amor que o mundo te da
+													
+													Meu alegre coração palpita
+													Por um universo de esperança
+													Me dê a mão
+													A magia nos espera
+													
+													Vou te amar por toda a minha vida
+													Vem comigo por este caminho
+													Me dê a mão
+													Pra fugir desta terrível escuridão</p>
+										</article>
+										<center>
+										<div class="row 150%">
+											<div class="6u 12u(mobile)">
+												<section class="box">
+													<header>
+														<h2>ICMC - Instituto de Ciências Matemáticas e da Computação</h2>
+													</header>
+													<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+													<p>Alguma descrição sobre interunidades</p>
+													<a href="#" class="button style1">Acesse</a>
+												</section>
+											</div>
+											<div class="6u 12u(mobile)">
+												<section class="box">
+													<header>
+														<h2>EESC - Escola de Engenharia de São Carlos</h2>
+													</header>
+													<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+													<p>alguma descriç'ao sobre interunidades</p>
+													<a href="#" class="button style1">Acesse</a>
+												</section>
+											</div>
+										</div>
+										</center>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			<!-- Footer -->
+				<div id="footer-wrapper" class="wrapper">
+					<div class="title">Pururu...</div>
+					<div id="footer" class="container">
+						<header class="style1">
+							<h2>Eaí bixão, já decorou o hino?</h2>
+							<p>
+								ôôôôôôôÔÔÔÔ...<br>
+								Nós somos! Lá de São Carlos!<br>
+								Viemos aqui! Pra zoneaaaaar!<br>
+								No esporte! Nós somos bosta<br>
+								Nosso negócio, é a cachaçaaaaa!<br>
+								<br>
+								A pinga, queremos com limããããoooo!!<br>
+								Mulheres/Homens com muito mais tesãããão!!<br>
+								Porém se a USP amada precisar da macacada<br>
+								puta merda que cagada!<br>
+								<br>
+
+								1! 2! 3! 4!
+								<br>
+
+								Como é legal! Calcular a integral!<br>
+								Mesmo sem grafiteeeeee<br>
+								Calculamos o limiteeee<br>
+								Amor de graçaaaa<br>
+								Pela cachaçaaaaa<br>
+								Não há quem possaaa<br>
+								Com a turma nossa!<br>
+
+								Engenharia U S P <br>
+								CAASO! CAASO!<br>
+								Botando pra fuder!<br>
+								Engenharia U S P<br> 
+								CAASO! CAASO!<br>
+								Botando pra fuder!<br>
+
+								ÔÔ FEDERAAAAL???<br>
+								XUPAAAAAAAA<br>
+								ÔÔ FEDERAAAAL???<br>
+								XUPAAAAAAAA<br>
+								IIII ÓÓÓ<br>
+								
+							</p>
+						</header>
+						<hr />
+						<div class="row 150%">
+								<center>
+						<div class="6u 12u(mobile)">
+			
+				<img src="images/saecomp1.png" width="70%" height="70%">
+				<div>
+				<div id="copyright">
+					<ul>
+						<li>&copy; Julio Ng</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+				</div>
+			</div>
+			</center>
+
+		</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
